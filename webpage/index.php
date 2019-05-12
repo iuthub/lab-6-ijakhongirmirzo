cfc8519303bf717753b9c387f0bf8a75ca9c43b7
ijakhongirmirzo <ijakhongirmirzo@gmail.com>
2019-05-13 00:00:05 +0500

    done

--- webpage/index.php
+++ webpage/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Validating Forms</title>
+ss		<title>Validating Forms</title>
 		<link href="style.css" type="text/css" rel="stylesheet" />
 	</head>
 	
